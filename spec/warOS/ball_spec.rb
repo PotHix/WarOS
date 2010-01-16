f33e309f9127b4e08e2668878b1c720267276648
PotHix <pothix@pothix.com>
2010-01-16 00:30:47 -0200

    Correcting a typo in files

--- spec/warOS/ball_spec.rb
+++ spec/warOS/ball_spec.rb
@@ -1,7 +1,7 @@
-require File.expand_path(File.dirname(__FILE__) + '/../lib/ship.rb')
+require File.dirname(__FILE__) + "/../spec_helper"
 
-describe Ship do
-  it 'should be the truth' do
-    Ship.truth.should be_true
+describe Ball do
+  it 'should instantiate a ball object with a image loaded' do
+    Ball.truth.should be_true
   end
 end
--- spec/warOS/ship_spec.rb
+++ spec/warOS/ship_spec.rb
@@ -1,7 +1,7 @@
-require File.expand_path(File.dirname(__FILE__) + '/../lib/ball.rb')
+require File.dirname(__FILE__) + "/../spec_helper"
 
-describe Ball do
-  it 'should instantiate a ball object with a image loaded' do
-    Ball.truth.should be_true
+describe Ship do
+  it 'should be the truth' do
+    Ship.truth.should be_true
   end
 end
